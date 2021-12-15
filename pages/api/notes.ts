f1a93aba134f6cb0cf51118ee08a70b2aad44b4e
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2021-12-14 22:47:58 -0600

    mongoDB api CRUD

---
+++ pages/api/notes.ts
@@ -0,0 +1,92 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { connectToDatabase } = require('../../lib/mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+export default async (req, res): Promise<any> => {
+  // return new Promise((resolve, reject) => {});
+  // res.json({ test: 'test' });
+  // switch the methods
+  switch (req.method) {
+    case 'GET': {
+      try {
+        const { db } = await connectToDatabase();
+        const notes = await db.collection('todolist').find({}).toArray();
+
+        console.log('data, ', notes);
+
+        res.status(200).json({ success: true, data: notes });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    }
+    case 'POST': {
+      try {
+        // connect to the database
+        const { db } = await connectToDatabase();
+        // add the note
+        await db.collection('todolist').insertOne(JSON.parse(req.body));
+        // return a message
+        return res.json({
+          message: 'Note added successfully',
+          success: true,
+        });
+      } catch (error) {
+        // return an error
+        return res.json({
+          message: new Error(error).message,
+          success: false,
+        });
+      }
+    }
+    case 'PUT': {
+      try {
+        // connect to the database
+        const { db } = await connectToDatabase();
+
+        // update the published status of the note
+        await db.collection('todolist').updateOne(
+          {
+            _id: new ObjectId(req.body),
+          },
+          { $set: { published: true } }
+        );
+
+        // return a message
+        return res.json({
+          message: 'Note updated successfully',
+          success: true,
+        });
+      } catch (error) {
+        // return an error
+        return res.json({
+          message: new Error(error).message,
+          success: false,
+        });
+      }
+    }
+    case 'DELETE': {
+      try {
+        // Connecting to the database
+        const { db } = await connectToDatabase();
+
+        // Deleting the post
+        await db.collection('todolist').deleteOne({
+          _id: new ObjectId(req.body),
+        });
+
+        // returning a message
+        return res.json({
+          message: 'Note deleted successfully',
+          success: true,
+        });
+      } catch (error) {
+        // returning an error
+        return res.json({
+          message: new Error(error).message,
+          success: false,
+        });
+      }
+    }
+  }
+};
